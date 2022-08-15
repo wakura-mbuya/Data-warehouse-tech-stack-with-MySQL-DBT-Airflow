de0616dba05e9aa89e656edb4b8335eee56a7066
Kennedy Wakura <wakura9@gmail.com>
2022-08-15 09:32:07 +0300

    add dbt models

---
+++ dbt_/models/traffic_models/dim_types.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='table') }}
+with source_data as (
+    select * from {{source("traffic_source_table", "source")}}
+),
+
+final as (
+    SELECT distinct
+    md5(types) as Id,
+    types FROM source_data
+)
+
+select * from final
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
